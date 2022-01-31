1e1b875da176f0144a45888a42415d13e3bdc3f9
Katerina Symiakaki <symiakaki@AD.IGB-BERLIN.DE>
2022-01-31 18:01:40 +0100

    well...

--- symiakaki_week03.R
+++ symiakaki_week03.R
@@ -1,67 +1,130 @@
+# sorry for being late
+
 library(tidyverse)
 library(rethinking)
 
 data("foxes")
-d <- foxes
+f <- foxes %>% 
+  mutate(A = scale(area),
+         F = scale(avgfood),
+         W = scale(weight),
+         G = scale(groupsize))
 
 # 1 ---------
 
-ggplot(d) +
-  geom_point(aes(x = area, y = avgfood))
-
-
-set.seed(13)
-N <- 100 # 100 lines
-a <- rnorm( N , .1 , .2 )
-b <- rlnorm( N , .2 , 1 )
-plot( NULL , xlim=range(d$area) , ylim=c(0,1.25) ,
-      xlab="area" , ylab="weight" )
-for ( i in 1:N ) curve( a[i] + b[i]*x,
-                        from=min(d$area) , to=max(d$area) , add=TRUE ,
-                        col=col.alpha("black",0.2) )
-
-
-area.av <- mean(d$area)
-
+#model
 m.AF <- quap(
   alist(
-    avgfood ~ dnorm(mi, sigma) ,  
-    mi <- a + b*(area - area.av) ,  
-    a ~ dnorm(.1 , .2) ,
-    b ~ dlnorm(.2 , 11) ,
+    F ~ dnorm(mi, sigma),  
+    mi <- a + b*A,  
+    a ~ dnorm(0, .2),
+    b ~ dlnorm(0, .5),
     sigma ~ dexp(1)
-  ) , data=d)
+  ), data=f)
+
+# prior predictive check
+prior <- extract.prior(m.AF)
+xseq <- c(-2,2)
+mi <- link(m.AF, post = prior, data=list(A=xseq))
+plot(NULL, xlim=xseq, ylim=xseq)
+for ( i in 1:100 ) lines(xseq, mi[i,], col=col.alpha("black",0.3))
+# they seem ok
+
+
 precis(m.AF)
 
+post <- extract.samples(m.AF, n = 100)
 
-# compute mi for each year of growth
-teritory <- seq(1, 5, .2)
-mi <- link(m.AF, data = data.frame(area = teritory))
+ggplot() +
+  geom_point(data =f, aes(x = A, y = F)) +
+  geom_abline(intercept = post$a, slope = post$b, colour = "forestgreen", alpha = .1) +
+  xlab("Area")+
+  ylab("Food")+
+  theme_minimal()
 
-plot(avgfood ~ area, d, type="n")
-for ( i in 1:100 ){
-  points( teritory , mi[i,] , pch=16 , col=col.alpha(rangi2,0.1) )}
-# bit of a heavy start and slow weight gain, but that's life
-
-
-# let's summarize mi
-mi.mean <- apply(mi, 2, mean)
-mi.PI <- apply(mi, 2, PI, prob=0.89)
-
-# plot raw data
-
-plot(avgfood ~ area, d, col=col.alpha(rangi2,0.5))
-# draw MAP line
-lines(teritory, mi.mean)
-# plot a shaded region for PI
-shade(mi.PI, teritory)
-
-# the food availeable to the foxed would increase if they expanded their territory
+# the food available to the foxes would increase if they expanded their territory
 
 # 2 ---------
 
-# to estimate the total effect of F on W we don't need to adjust on any variable
-#to estimate the direct effect, we need to adjust on G
+# To estimate the total effect of F on W we don't need to adjust on any variable
+
+ggplot(f) +
+  geom_point(aes(x = F, y = W))
+# that's a very difficult line to draw
+
+#model
+m.FW <- quap(
+  alist(
+    W ~ dnorm(mi, sigma),  
+    mi <- a + b*F,  
+    a ~ dnorm(0 , 2),
+    b ~ dlnorm(0 , .5),
+    sigma ~ dexp(1)
+  ), data=f)
+
+
+# prior predictive check
+prior <- extract.prior(m.FW)
+xseq <- c(-2,2)
+mi <- link(m.FW, post = prior, data=list(A=xseq))
+plot(NULL, xlim=xseq, ylim=xseq)
+for ( i in 1:100 ) lines(xseq, mi[i,], col=col.alpha("black",0.3))
+precis(m.FW)
+# just about right
+# the availability of food doesn't seem to affect the average fox weight
+
+
+#To estimate the direct effect, we need to adjust on G
+
+# model
+m.FWG <- quap(
+  alist(
+    W ~ dnorm(mi, sigma),  
+    mi <- a + bF*F + bG*G,  
+    a ~ dnorm(0 , .2),
+    bF ~ dnorm(0 , .5),
+    bG ~ dnorm(0 , .5),
+    sigma ~ dexp(1)
+  ), data=f)
+
+precis(m.FWG)
+# now the effect of food availability on fox weight is positive 
+# but the effect of group size on weight is negative
+
+post <- extract.samples(m.FWG, 100)
+
+ggplot() +
+  geom_point(data =f, aes(x = F, y = W)) +
+  geom_abline(intercept = post$a, slope = post$bF, colour = "forestgreen", alpha = .2) +
+  xlab("Food")+
+  ylab("Weight")+
+  theme_minimal()
+
+ggplot() +
+  geom_point(data =f, aes(x = G, y = W)) +
+  geom_abline(intercept = post$a, slope = post$bG, colour = "lightblue", alpha = .3) +
+  xlab("Group size")+
+  ylab("Weight")+
+  theme_minimal()
+
+
+# It makes sense that the group size has a negative effect on weight, because for a given amount 
+# food in a given area, a larger group of foxes would "starve" compared to a smaller one. On the 
+# other hand, the group size should also affect the territory, as a larger group could claim a 
+# larger area. So the DAG should have one more arrow from G to A. 
+
+
+# 3 ----------
+
+# In this DAG there are two back doors to X, one from S and one from A. So to estimate the effect 
+# of X to Y we should adjust on both those parameters.
+
+# The coefficient of X is not affected by the unobserved variable. There is a collider path opening 
+# when we adjust on S.
+
+  
+
+
 
 
 
