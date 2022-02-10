e277edd7ba09f6161a4b7653d9c51a4c37a88c39
Katerina Symiakaki <symiakaki@AD.IGB-BERLIN.DE>
2022-02-10 13:28:47 +0100

    maybe done?

--- symiakaki_week05.R
+++ symiakaki_week05.R
@@ -3,30 +3,19 @@ library(rethinking)
 
 # 1 --------
 library(dagitty)
-data("NWOGrants")
 
 award.dag <- dagitty("dag{
   gender -> discipline -> awards;
-  gender -> awards;
-  gender -> applications -> awards;
-  gender -> discipline -> applications -> awards}")
+  gender -> awards}")
 
 plot(award.dag)
-adjustmentSets(award.dag, "F", "W")
-impliedConditionalIndependencies( fx_dag )
 
+data("NWOGrants")
 d <- NWOGrants %>% 
   mutate(G = ifelse(gender=="f", 1, 2),
          D = as.integer(discipline))
-# 
-# # generative example
-# n <- 100
-# G <- sample(1:2, size = n, replace = T)
-# D <- rbern(n, ifelse(G==1, 0.3, 0.8)) +1
-# aw.rate <- matrix(c(0.1, 0.3, 0.1, 0.3), nrow = 2)
-# A <- rbern(n, aw.rate[D, G])
 
-
+# I have to set this otherwise the models don't run
 set_cmdstan_path('C:\\Users\\symiakaki\\Documents\\R\\R-4.1.2\\library\\cmdstan')
 
 # total effect of gender
@@ -35,19 +24,20 @@ m <- ulam(
     awards ~ dbinom(applications, p),
     logit(p) <- a[G],
     a[G] ~ dnorm( 0 , 1 )
-  ), data=d, chains = 3, cores = 3 )
+  ), data=d, chains = 4, cores = 4 )
 
 precis( m , depth=2 )
-trankplot(m)
+trankplot(m, n_cols = 2)
 dev.off()
 
 post <- extract.samples(m)
 pra_1 <- inv_logit(post$a[,1]) # f
 pra_2 <- inv_logit(post$a[,2]) # m
-diff <- pra_2 - pra_1
+diff <- pra_2 - pra_1 # m-f
 dens(diff, lwd = 2, col= 2, xlab = "Gender contrast")
+abline(v= 0)
 
-# 
+# the highest density is in positive values so men seem to be favored over women - once again
 
 # 2 --------
 
@@ -57,15 +47,27 @@ m2 <- ulam(
     awards ~ dbinom(applications, p),
     logit(p) <- a[G, D],
     matrix[G, D]:a ~ dnorm( 0 , 1 )
-  ), data=d, chains = 3, cores = 3 )
+  ), data=d, chains = 4, cores = 4 )
+beepr::beep()
 
 precis( m2 , depth=3 )
 trankplot(m2)
 dev.off()
 
+# app_dep <-sapply(1:9, function(i) sum(d$applications[d$D==i]))
+# p1 <- link(m2, data = )
+# p2
+
 post <- extract.samples(m2)
 pra <- inv_logit(post$a)
-diff_D <- sapply(1:9, function(i) pra[,1, i] - pra[,2,i])
+diff_D <- sapply(1:9, function(i) pra[,2, i] - pra[,1,i])
 w <- xtabs(d$applications ~ d$D) / sum(d$applications)
-plot(NULL, xlim = c(-.2, .3), ylim = c(0,25), ylab = "Density", xlab = "Gender contrast")
+
+plot(NULL, xlim = c(-.25, .25), ylim = c(0,25), ylab = "Density", xlab = "Gender contrast")
 for (i in 1:9) dens(diff_D[,i], lwd = 1+ 20*w[i], col = 1+i, add = T)
+
+# 3 -----
+
+# As we see from the weighted effects of the different departments, two departments 
+# are mostly driving the mean posterior distribution of the total effect. 
+# 
